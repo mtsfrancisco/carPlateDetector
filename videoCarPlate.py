b4a1831653d5136a9b295e91cfd0aae0543b8114
Matheus <matheusdabmlj@gmail.com>
2024-05-23 13:28:14 -0300

    AVANÇO

--- videoCarPlate.py
+++ videoCarPlate.py
@@ -3,55 +3,50 @@ from matplotlib import pyplot as plt
 import numpy as np
 import imutils
 import easyocr
+from ultralytics import YOLO
+import pytesseract
 
-video = cv2.VideoCapture('traffic.mp4')
+plate_model = YOLO(r"license_plate_detector.pt")
 
 
-def plateDetect(img):
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    bfilter = cv2.bilateralFilter(gray, 11, 17, 17) #Noise reduction
-    edged = cv2.Canny(bfilter, 30, 200) #Edge detection
-    keypoints = cv2.findContours(edged.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    contours = imutils.grab_contours(keypoints)
-    contours = sorted(contours, key=cv2.contourArea, reverse=True)[:10]
+video = cv2.VideoCapture('Watch catastrophic parking fail as Jaguar driver hits two parked cars _ SWD Media.mp4')
+easyocr_reader = easyocr.Reader(['en'])
+pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
 
-    global location
-    location = None
-    for contour in contours:
-        approx = cv2.approxPolyDP(contour, 10, True)
-        if len(approx) == 4:
-            location = approx
-            break
 
-    maskk = np.zeros(gray.shape, np.uint8)
-    new_image = cv2.drawContours(maskk, [location], 0,255, -1)
-    new_image = cv2.bitwise_and(img, img, mask=maskk)
 
-    (x,y) = np.where(maskk==255)
-    (x1, y1) = (np.min(x), np.min(y))
-    (x2, y2) = (np.max(x), np.max(y))
-    cropped_image = gray[x1:x2+1, y1:y2+1]
-    return cropped_image
 
-    reader = easyocr.Reader(['en'])
-    result = reader.readtext(cropped_image)
 
-    if result == []:
-        return img
-    
-    text = result[0][-2]
-    font = cv2.FONT_HERSHEY_SIMPLEX
-    res = cv2.putText(img, text=text, org=(approx[0][0][0], approx[1][0][1]+60), fontFace=font, fontScale=1, color=(0,255,0), thickness=2, lineType=cv2.LINE_AA)
-    res = cv2.rectangle(img, tuple(approx[0][0]), tuple(approx[2][0]), (0,255,0),3)
-    return res
 
+
+frame_rate = video.get(cv2.CAP_PROP_FPS)
+interval = int(frame_rate)
 
 while True:
     ret, frame = video.read()
-    new_frame = plateDetect(frame)
-    cv2.imshow('Video', new_frame)
+    if not ret:
+        break
+
+    current_frame = int(video.get(cv2.CAP_PROP_POS_FRAMES))
+    if current_frame % interval != 0:
+        continue
+
+    plate_detections = plate_model(frame)[0]
+
+    for det in plate_detections.boxes.data.tolist():
+        x1, y1, x2, y2, score, class_id = det
+        cv2.rectangle(frame, (int(x1), int(y1)), (int(x2), int(y2)), (0, 0, 255), 2)
+
+        plate = frame[int(y1):int(y2), int(x1):int(x2)]
+        plate = cv2.cvtColor(plate, cv2.COLOR_BGR2GRAY)
+        _, binary_image = cv2.threshold(plate, 135 ,255, cv2.THRESH_BINARY_INV)
+
+        plate_text = pytesseract.image_to_string(binary_image, config='--psm 7')
+        cv2.putText(frame, plate_text, (int(x1), int(y1 - 10)), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+
+    cv2.imshow('frame', frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
-cv2.destroyAllWindows()
 video.release()
+cv2.destroyAllWindows()
