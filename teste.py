43b92df8900637b5e281f5c918feab30e560d451
mtsfrancisco <matheusdabmlj@gmail.com>
2024-05-27 12:46:00 -0300

    asd

--- teste.py
+++ teste.py
@@ -1,28 +1,50 @@
 import cv2
 from matplotlib import pyplot as plt
 import numpy as np
-import pytesseract
+
+# Carregar a imagem
+image = cv2.imread('carro.jpeg', 0)  # Carregar a imagem em escala de cinza
+
+# Aplicar filtro de Prewitt vertical
+prewitt_vertical = np.array([[1, 0, -1],
+                              [1, 0, -1],
+                              [1, 0, -1]])
+filtered_image = cv2.filter2D(image, -1, prewitt_vertical)
 
 
-img = cv2.imread("APENASAPLACA.jpeg")
-img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-img = cv2.resize(img, (int(img.shape[1] * 5), int(img.shape[0] * 5)))
-img = img[35:-30, 30:-30]
+# Aplicar filtro de mediana
+filtered_image = cv2.medianBlur(filtered_image, 3)
+
+# Calcular histograma da intensidade das linhas
+histogram = np.sum(filtered_image, axis=1)
 
 
+# Aplicar filtro de média 9x9 para normalização dos dados
+normalized_histogram = cv2.blur(histogram, (9, 9))
+plt.plot(normalized_histogram)
+plt.show()
 
-img = cv2.medianBlur(img, 3)
-#img = cv2.GaussianBlur(img, (5, 5), 0)
-img = cv2.threshold(img, 0,255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
+# Limiarização (thresholding) para identificar os picos
+thresholded_histogram = np.where(normalized_histogram > 60, normalized_histogram, 0)
 
+# Descartar 20% das extremidades do histograma
+discard_percentage = 20
+discard_pixels = int(len(thresholded_histogram) * (discard_percentage / 100))
+thresholded_histogram[:discard_pixels] = 0
+thresholded_histogram[-discard_pixels:] = 0
 
-kernel = np.ones((3,3), np.uint8)
-img = cv2.dilate(img, kernel, iterations=2)
+# Identificar pico de maior valor
+peak_index = np.argmax(thresholded_histogram)
 
-texto = pytesseract.image_to_string(img, config='--psm 7')
-print("aaaa")
-print(texto)
+# Determinar intervalo candidato da região da placa
+plate_region_start = max(0, peak_index - 20)
+plate_region_end = min(len(image), peak_index + 20)
 
-cv2.imshow('plate2', img)
+# Recortar a região da placa candidata
+plate_candidate = image[plate_region_start:plate_region_end, :]
+
+# Mostrar imagem resultante com a região da placa candidata destacada
+cv2.imshow('Plate Candidate', plate_candidate)
 cv2.waitKey(0)
+cv2.destroyAllWindows()
 
--- videoCarPlate.py
+++ videoCarPlate.py
@@ -8,25 +8,25 @@ import pytesseract
 
 plate_model = YOLO(r"license_plate_detector.pt")
 
-video = cv2.VideoCapture('Watch catastrophic parking fail as Jaguar driver hits two parked cars _ SWD Media.mp4')
+video = cv2.VideoCapture('2103099-hd_1920_1080_30fps.mp4')
+#video = cv2.VideoCapture(0)
+
 
 # QUANDO FOR USAR NO WINDOWS
 # pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
 
 
-#frame_rate = video.get(cv2.CAP_PROP_FPS)
-#interval = int(frame_rate)
+# frame_rate = video.get(cv2.CAP_PROP_FPS)
+# interval = int(frame_rate)
 
 kernel = np.ones((3,3), np.uint8)
+plate_texts = []
 
 while True:
     ret, frame = video.read()
-    # if not ret:
-    #     break
+    if not ret:
+        break
 
-    # current_frame = int(video.get(cv2.CAP_PROP_POS_FRAMES))
-    # if current_frame % interval != 0:
-    #     continue
 
     plate_detections = plate_model(frame)[0]
 
@@ -34,22 +34,50 @@ while True:
         x1, y1, x2, y2, score, class_id = det
         cv2.rectangle(frame, (int(x1), int(y1)), (int(x2), int(y2)), (0, 0, 255), 2)
         plate = frame[int(y1):int(y2), int(x1):int(x2)]
+        plate = cv2.resize(plate, (int(plate.shape[1] * 5), int(plate.shape[0] * 5)))
+
         plate = cv2.cvtColor(plate, cv2.COLOR_BGR2GRAY)
         
-        plate = cv2.resize(plate, (int(plate.shape[1] * 5), int(plate.shape[0] * 5)))
-        plate = plate[55:-30, 45:-45]
-        plate = cv2.medianBlur(plate, 3)
-
-        _, binary_image = cv2.threshold(plate, 0 ,255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
-        binary_image = cv2.dilate(binary_image, kernel, iterations=2)
         
+ 
+        #plate = plate[int(-plate.shape[1]*0.20):int(plate.shape[1]*0.250), int(plate.shape[0]*0.45):int(-plate.shape[0]*0.25)]
+        
+
+
+        # cv2.imshow('plate', plate)
+        # cv2.waitKey(0)
+
+        #plate = plate[55:-30, 45:-45]
+        
+        #plate = cv2.medianBlur(plate, 3)
+        _, binary_image = cv2.threshold(plate, 0 ,255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)   
+        
+        #cv2.imshow('plate', binary_image)
+        #cv2.waitKey(0)
+
+        binary_image = cv2.erode(binary_image, kernel, iterations = 2)
+        #binary_image = cv2.dilate(binary_image, kernel, iterations = 2)
+        
+        # cv2.imshow('plate', binary_image)
+        # cv2.waitKey(0)
+        # cv2.destroyAllWindows()
+
+
+        plate_text = pytesseract.image_to_string(binary_image, config='--psm 7')
+        filtrado = ''.join(filter(lambda x: x.isalnum(), plate_text))
+        
+        #plate_texts.append(str(filtrado))
+        #print(plate_texts)
+        # if len(plate_texts) % 4 == 0:
+        #     most_common_plate_text = max(set(plate_texts), key=plate_texts.count)
+        #     print(most_common_plate_text)
+        #     cv2.putText(frame, most_common_plate_text, (int(x1), int(y1 - 10)), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+        cv2.putText(frame, filtrado, (int(x1), int(y1 - 10)), cv2.FONT_HERSHEY_SIMPLEX, 3, (0, 0, 255), 2)
         
         cv2.imshow('plate', binary_image)
         cv2.waitKey(0)
-        cv2.destroyAllWindows()
 
-        plate_text = pytesseract.image_to_string(binary_image, config='--psm 7')
-        cv2.putText(frame, plate_text, (int(x1), int(y1 - 10)), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+        #cv2.putText(frame, filtrado, (int(x1), int(y1 - 10)), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
 
     cv2.imshow('frame', frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
